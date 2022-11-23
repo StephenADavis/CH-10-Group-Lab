0b0b912b7c8d4d527dd413ac06f8cd5951debf83
Stephen Davis <StephenDavisOCCC@users.noreply.github.com>
2022-11-23 11:33:38 -0500

    Update script.js
    removed unneeded script

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ var descriptionSource = document.getElementById("despsrc");
 //Hamburger menu function
 function hamburger() {
     var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
+    var logo = document.getElementById("collegelogo");
     if (menu.style.display === "block" && logo.style.display === "none") {
       menu.style.display = "none";
       logo.style.display = "block";
@@ -20,36 +20,4 @@ function hamburger() {
       menu.style.display = "block";
       logo.style.display = "none";
     }
-  }
-
-  //Function to display the burpees example video
-  function burpees() {
-    videoSource.src = "media/burpees.mp4";
-    descriptionSource.src = "media/burpees-descriptions.vtt";
-    video.style.display = "block";
-    video.load();
-  }
-
-  //Function to display the plank example video
-  function plank() {
-    videoSource.src = "media/plank.mp4";
-    descriptionSource.src = "media/plank-descriptions.vtt";
-    video.style.display = "block";
-    video.load();
-  }
-
-  //Function to display the mountain climbers example video
-  function mountain() {
-    videoSource.src = "media/mc.mp4";
-    descriptionSource.src = "media/mountain-descriptions.vtt";
-    video.style.display = "block";
-    video.load();
-  }
-
-  //Function to display a promo Code
-  function discount() {
-    var promo = document.getElementById("special");
-    promo.firstChild.nodeValue = "Promo Code: D25START";
-    promo.style.color = "#ff0000";
-    promo.style.fontsize = "2em";
   }
